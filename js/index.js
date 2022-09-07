5df136bd4d42b30851bc442a8580ad5b5ae237fe
Lipika <lipikadalai3@gmail.com>
2022-09-07 21:57:55 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+function selectItem(e) {
+    removeBorder()
+    removeShow();
+    //add border
+    this.classList.add('tab-border')
+    const tabshowcontent = document.querySelector(`#${this.id}-content`);
+    tabshowcontent.classList.add('show');
+}
+
+tabItems.forEach(item =>item.addEventListener('click',selectItem));
